e829eca39a8e0e7fc22d486015ab698a2f32d290
BrunoDavies <brunodavies@gmail.com>
2021-01-18 23:28:53 +0000

    added test to check you can change greeter's message

--- greeter/test/greeter_test.js
+++ greeter/test/greeter_test.js
@@ -1,19 +1,32 @@
 const GreeterContract = artifacts.require("Greeter");
 
-
 contract("Greeter", () => {
-	it("has been deployed successfully", async () => {
-		const greeter = await GreeterContract.deployed();
-		assert(greeter, "contract was not deployed");
-	});
+  it("has been deployed successfully", async () => {
+    const greeter = await GreeterContract.deployed();
+    assert(greeter, "contract failed to deploy");
+  });
+
+  describe("greet()", () => {
+    it("returns 'Hello, World!'", async () => {
+      const greeter = await GreeterContract.deployed();
+      const expected = "Hello, World!";
+      const actual = await greeter.greet();
+
+      assert.equal(actual, expected, "greeted with 'Hello, World!'");
+    });
+  });
 });
 
-describe("greet()", () => {
-  it("returns 'Hello, World!'", async () => {
-    const greeter = await GreeterContract.deployed();
-    const expected = "Hello, World!";
-    const actual = await greeter.greet();
+contract("Greeter: update greeting", () => {
+  describe("setGreeting(string)", () => {
+    it("sets greeting to passed in string", async () => {
+      const greeter = await GreeterContract.deployed()
+      const expected = "Hi there!";
 
-    assert.equal(actual, expected, "greeted with 'Hello, World!'");
+      await greeter.setGreeting(expected);
+      const actual = await greeter.greet();
+
+      assert.equal(actual, expected, "greeting was not updated");
+    });
   });
 });
