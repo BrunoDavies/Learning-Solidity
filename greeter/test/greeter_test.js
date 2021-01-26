7a22300055dd18d48870dbce0d7d922ade1b1fdb
Bruno Davies <49724107+BrunoDavies@users.noreply.github.com>
2021-01-26 09:47:56 +0000

    Tests to check only owner can change greeting

--- greeter/test/greeter_test.js
+++ greeter/test/greeter_test.js
@@ -1,6 +1,38 @@
 const GreeterContract = artifacts.require("Greeter");
 
 contract("Greeter", (accounts) => {
+  describe("setGreeting(string)", () => {
+    describe("when message is sent by the owner", () => {
+      it("sets greeting to passed in string", async () => {
+        const greeter = await GreeterContract.deployed;
+        const expected = "The owner changed the message";
+
+        await greeter.setGreeting(expected);
+        const actual = await greeter.greet();
+
+        assert.equal(actual, expected, "greeting updated");
+      });
+    });
+
+    describe("when message is sent by another account", () => {
+      it("does not set the greeting", async () => {
+        const greeter = await GreeterContract.deployed;
+        const expected = await greeter.greet();
+
+        try {
+          await greeter.setGreeting("Not the owner", {from: accounts[1]});
+        } catch(err){
+          const errorMessage = "Ownable: caller is not the owner"
+          //this is how the book does it but hardcoding what you think the error message should be
+          //is bad practice
+          assert.equal(err.reason, errorMessage, "greeting should not update");
+          return;
+        }
+        assert(false, "greeting was updated via non-owner");
+      });
+    });
+  })
+
   it("has been deployed successfully", async () => {
     const greeter = await GreeterContract.deployed();
     assert(greeter, "contract failed to deploy");
