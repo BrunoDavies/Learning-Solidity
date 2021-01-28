d6cda877002bd3a8cba343b09bd89a6428385954
BrunoDavies <brunodavies@gmail.com>
2021-01-28 16:36:00 +0000

    configured tests and truffle solc for library

--- greeter/test/greeter_test.js
+++ greeter/test/greeter_test.js
@@ -1,67 +1,70 @@
 const GreeterContract = artifacts.require("Greeter");
 
 contract("Greeter", (accounts) => {
-  describe("setGreeting(string)", () => {
-    describe("when message is sent by the owner", () => {
-      it("sets greeting to passed in string", async () => {
-        const greeter = await GreeterContract.deployed;
-        const expected = "The owner changed the message";
-
-        await greeter.setGreeting(expected);
-        const actual = await greeter.greet();
-
-        assert.equal(actual, expected, "greeting updated");
-      });
+    describe("deployment", () => {
+        it("has been deployed successfully", async () => {
+            const greeter = await GreeterContract.deployed();
+            assert(greeter, "contract failed to deploy");
+        });
     });
 
-    describe("when message is sent by another account", () => {
-      it("does not set the greeting", async () => {
-        const greeter = await GreeterContract.deployed;
-        const expected = await greeter.greet();
+    describe("greet()", () => {
+        it("returns 'Hello, World!'", async () => {
+            const greeter = await GreeterContract.deployed();
+            const expected = "Hello, World!";
+            const actual = await greeter.greet();
 
-        try {
-          await greeter.setGreeting("Not the owner", {from: accounts[1]});
-        } catch(err){
-          const errorMessage = "Ownable: caller is not the owner"
-          //this is how the book does it but hardcoding what you think the error message should be
-          //is bad practice
-          assert.equal(err.reason, errorMessage, "greeting should not update");
-          return;
-        }
-        assert(false, "greeting was updated via non-owner");
-      });
-    });
-  })
-
-  it("has been deployed successfully", async () => {
-    const greeter = await GreeterContract.deployed();
-    assert(greeter, "contract failed to deploy");
-  });
-
-  describe("greet()", () => {
-    it("returns 'Hello, World!'", async () => {
-      const greeter = await GreeterContract.deployed();
-      const expected = "Hello, World!";
-      const actual = await greeter.greet();
-
-      assert.equal(actual, expected, "greeted with 'Hello, World!'");
-    })
-  });
-
-  describe("owner()", () => {
-    it("returns the address of the owner", async () => {
-      const greeter = await GreeterContract.deployed();
-      const owner = await greeter.owner();
-
-      assert(owner, "the current owner");
+            assert.equal(actual, expected, "greeted with 'Hello, World!'");
+        })
     });
 
-    it("matches the address that originally deployed the contract", async () => {
-      const greeter = await GreeterContract.deployed();
-      const owner = await greeter.owner();
-      const expected = accounts[0];
+    describe("owner()", () => {
+        it("returns the address of the owner", async () => {
+            const greeter = await GreeterContract.deployed();
+            const owner = await greeter.owner();
+            
+            assert(owner, "the current owner");
+        });
 
-      assert.equal(owner, expected, "matches address used to deploy contract");
+        it("matches the address that originally deployed the contract", async () => {
+            const greeter = await GreeterContract.deployed();
+            const owner = await greeter.owner();
+            const expected = accounts[0];
+            
+            assert.equal(owner, expected, "matches address used to deploy contract");
+        });
     });
-  });
-})
+});
+
+contract("Greeter: update greeting", (accounts) => {
+    describe("setGreeting(string)", () => {
+        describe("when message is sent by the owner", () => {
+            it("sets greeting to passed in string", async () => {
+                const greeter = await GreeterContract.deployed()
+                const expected = "The owner changed the message";
+
+                await greeter.setGreeting(expected);
+                const actual = await greeter.greet();
+
+                assert.equal(actual, expected, "greeting was not updated");
+            });
+        });
+
+        describe("when message is sent by another account", () => {
+            it("does not set the greeting", async () => {
+                const greeter = await GreeterContract.deployed()
+                const expected = await greeter.greet();
+
+                try {
+                    await greeter.setGreeting("Not the owner", { from: accounts[1] });
+                } catch(err) {
+                    const errorMessage = "Ownable: caller is not the owner"
+                    //this is how the book does it but hard coding the error message is not good practice
+                    assert.equal(err.reason, errorMessage, "greeting should not update");
+                    return;
+                }
+                assert(false, "greeting should not update");
+            });
+        });
+    });
+});
